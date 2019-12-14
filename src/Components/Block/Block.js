b6a7b213d95da8d4f33131a6ec925f0c815f9b05
qiqetes <qiqete@gmail.com>
2019-12-14 01:16:17 +0100

    Added holds styling

--- src/Components/Block/Block.js
+++ src/Components/Block/Block.js
@@ -3,21 +3,37 @@ import Hold from './Hold/Hold'
 import './Block.scss';
 
 
-const Block = ({ blockData }) => (
-    <div className="block">
+const Block = ({ blockData }) => {
+    const difficultyColor = () => {
+        let color;
 
-        <h1>{blockData.name}</h1>
-        <div>
-            <div className="block-img">
-                <img src={blockData.url} alt="" />
-            </div>
-            <div className="block-holds">
-                {blockData.holds.map((hold, i) => {
-                    return <Hold key={i} type={hold.type} x={hold.x} y={hold.y} size={hold.size}></Hold>
-                })}
+        // TODO: add a better color gradding function
+        switch (blockData.grade) {
+            case '5a':
+                color = '#c9f257'; break;
+            case '6b':
+                color = '#f00c3a'; break;
+        }
+        console.log(color);
+        return color;
+    }
+
+    return (
+        <div className="block">
+
+            <h1>{blockData.name}</h1>
+            <div>
+                <div className="block-img">
+                    {/* <img src={blockData.url} alt="" /> */}
+                </div>
+                <div className="block-holds">
+                    {blockData.holds.map((hold, i) => {
+                        return <Hold key={i} type={hold.type} x={hold.x} y={hold.y} size={hold.size} color={difficultyColor()}></Hold>
+                    })}
+                </div>
             </div>
         </div>
-    </div>
-);
+    )
+};
 
 export default Block;
--- src/Components/Block/Hold/Hold.js
+++ src/Components/Block/Hold/Hold.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
+import './Hold.scss'
 
-const Hold = ({ type, x, y, size }) => (
-    <div></div>
-);
+const Hold = ({ type, x, y, size, color }) => {
+    const circleStyle = {
+        width: `${size}px`,
+        height: `${size}px`,
+        top: `${y}px`,
+        left: `${x}px`,
+        borderColor: color,
+    }
+
+    return (
+        <div>
+            <div className="circle" style={circleStyle}>
+
+            </div>
+        </div>
+    )
+};
 
 export default Hold;
