f43f4a213bfbcb5a99507439daeb508aeec3191d
qiqetes <qiqete@gmail.com>
2019-12-14 14:15:26 +0100

    hold icons formula done, todo: add the icon svgs

--- src/Components/Block/Hold/Hold.js
+++ src/Components/Block/Hold/Hold.js
@@ -2,21 +2,43 @@ import React, { Component } from 'react';
 import './Hold.scss'
 
 const Hold = ({ type, x, y, size, color }) => {
+    const borderRadius = type == 'top' ? 12 : 6;
     const circleStyle = {
         width: `${size}px`,
         height: `${size}px`,
-        bottom: `calc(${y}% - ${size / 2}px`,
-        left: `calc(${x}% - ${size / 2}px`,
-        borderColor: color,
+        bottom: `calc(${y}% - ${size / 2}px - ${borderRadius}px)`,
+        left: `calc(${x}% - ${size / 2}px - ${borderRadius}px)`,
+        border: `${borderRadius}px solid ${color}`
     }
 
     return (
-        <div>
-            <div className="circle" style={circleStyle}>
-
-            </div>
+        // Types to handle: hand, hands, foot, foots, optional, top
+        <div className="circle" style={circleStyle}>
+            {
+                type == "hand" ? <HoldIcon parentSize={size / 2} color={color}></HoldIcon> : null
+            }
         </div>
     )
 };
+
+const HoldIcon = ({ type, color, parentSize }) => {
+
+    const iconRadius = 24; // TODO: better in percentage
+    const holdIconStyle = {
+        // R - 2r - cos(45) * R + r - r * cos(45)
+        top: `calc(${parentSize}px - 2 * ${iconRadius}px - 0.7071 * ${parentSize}px + ${iconRadius}px - ${iconRadius}px * 0.7071 )`,
+        left: `calc(${parentSize}px - 2 * ${iconRadius}px - 0.7071 * ${parentSize}px + ${iconRadius}px - ${iconRadius}px * 0.7071 )`,
+        backgroundColor: `${color}`,
+        width: `${iconRadius * 2}px`,
+        height: `${iconRadius * 2}px`
+    }
+
+
+    return (
+        <div className="holdIcon" style={holdIconStyle}>
+
+        </div>
+    );
+}
 
 export default Hold;
